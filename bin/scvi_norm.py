af19c0489fad3cf466f84859b2f73733cd12d10a
Jura Pintar <jpintar@users.noreply.github.com>
2024-06-23 16:24:44 -0400

    Misc. revisions to scVI code
    1. Set Torch precision and scVI random seed for better reproducibility
    2. Change HVG selection to seurat_v3, which operates on untransformed counts (like scVI)
    3. Add early stopping to model training
    4. Compute integrated normalised expression in addition to unintegrated
    4. Save scVI outputs to full data-set, rather than HVG subset

--- bin/scvi_norm.py
+++ bin/scvi_norm.py
@@ -2,51 +2,64 @@ import os
 import scvi
 import scanpy as sc
 import argparse
+import torch
 
 parser = argparse.ArgumentParser(
     description="wrapper for DoubletDetection for doublet detection from transcriptomic data.")
-parser.add_argument("-i", "--input", required = True, default = "", help = "The input h5ad file")
-parser.add_argument("-o", "--output", required = False, help = "The output h5ad file")
-parser.add_argument('--n_latent', default=10, type = int,  required=False, help='Number of latent dimensions.')
-parser.add_argument('--n_top_genes', default=100, type = int, required=False, help='Number of top genes.')
+parser.add_argument("-i", "--input", required=True, default="", help="The input h5ad file")
+parser.add_argument("-o", "--output", required=True, help="The output h5ad file")
+parser.add_argument("--n_latent", default=10, type=int, required=False, help="Number of latent dimensions.")
+parser.add_argument("--n_top_genes", default=1000, type=int, required=False, help="Number of top genes.")
 
 args = parser.parse_args()
 
+torch.set_float32_matmul_precision("high")
+
 adata = sc.read_h5ad(args.input)
-adata.layers['without_log'] = adata.X
+adata.layers["counts"] = adata.X
 adata.raw = adata
 
-sc.pp.log1p(adata, base=2)
-
-adata.obs['sample_name'] = adata.obs['sample_name'].astype('category')
+adata.obs["sample_name"] = adata.obs["sample_name"].astype("category")
 
 sc.pp.highly_variable_genes(
     adata,
-    n_top_genes=args.n_top_genes, 
-    subset=True,
-    flavor="seurat", 
+    layer="counts",
+    n_top_genes=args.n_top_genes,
+    flavor="seurat_v3", 
     batch_key="sample_name"
 )
 
+adata_hvg = adata[:, adata.var["highly_variable"]].copy()
+
 scvi.model.SCVI.setup_anndata(
-    adata,
-    batch_key = 'sample_name',
-    categorical_covariate_keys = ['sample_name'],
+    adata_hvg,
+    layer="counts",
+    batch_key="sample_name",
     continuous_covariate_keys=["mito_frac"]
 )
 
-model = scvi.model.SCVI(adata, n_latent = args.n_latent)
+model = scvi.model.SCVI(adata_hvg, n_latent=args.n_latent)
 
-model.train()
+scvi.settings.seed = 18591124
+model.train(
+    early_stopping=True,
+    early_stopping_monitor='reconstruction_loss_validation'
+)
 
+scvi.settings.seed = 18591124
 latent = model.get_latent_representation()
 
-adata.obsm["X_scVI"] = latent
+scvi.settings.seed = 18591124
+denoised = model.get_normalized_expression(library_size=1e4)
 
-denoised = model.get_normalized_expression(adata, library_size=1e4)
-
-adata.layers["scvi_normalized"] = model.get_normalized_expression(
-    library_size=10e4
+scvi.settings.seed = 18591124
+integrated = model.get_normalized_expression(
+    transform_batch=adata.obs["sample_name"].unique().tolist(),
+    library_size=1e4
 )
+
+adata.obsm["X_scVI"] = latent
+adata.obsm["scvi_normalized"] = denoised
+adata.obsm["scvi_normalized_integrated"] = integrated
         
 adata.write_h5ad(args.output)
