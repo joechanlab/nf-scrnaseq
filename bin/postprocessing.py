6db0d80b595ccd18d18e941f6cda729f4cb393ce
Mamie Wang <wangm10@mskcc.org>
2024-07-15 10:31:56 -0400

    Corrected a bug in finding metadata column names

--- bin/postprocessing.py
+++ bin/postprocessing.py
@@ -73,16 +73,18 @@ parser.add_argument(
     action=argparse.BooleanOptionalAction,
     help="Use SCVI latent variable instead of PCA.",
 )
-parser.add_argument("--metadata", required=False, default="", help="Metadata to be added to obs.")
+parser.add_argument(
+    "--metadata", required=False, default="", help="Metadata to be added to obs."
+)
 args = parser.parse_args()
 
 adata = sc.read_h5ad(args.input_h5ad)
 
 if not args.metadata == "":
     metadata = pd.read_csv(args.metadata)
-    new_cols = [x for x in metadata.cols if x not in adata.obs.columns]
-    intersect_cols = [x for x in metadata.cols if x in adata.obs.columns]
-    metadata = adata.obs.merge(metadata, how = 'left', on = intersect_cols)
+    new_cols = [x for x in metadata.columns if x not in adata.obs.columns]
+    intersect_cols = [x for x in metadata.columns if x in adata.obs.columns]
+    metadata = adata.obs.merge(metadata, how="left", on=intersect_cols)
     assert metadata.shape[0] == adata.shape[0]
     for new_col in new_cols:
         adata.obs[new_col] = metadata[new_col]
